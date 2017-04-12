46a6dd86aa50569493b09b703bca544fc05c7378
MaxioN <maxion03@gmail.com>
2017-04-12 13:26:52 +0300

    Web

---
+++ web/src/main/java/com/netcracker/kutz/Controller.java
@@ -0,0 +1,24 @@
+package com.netcracker.kutz;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Егор on 12.04.17.
+ */
+
+//todo
+public class Controller extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        super.doGet(httpServletRequest, httpServletResponse);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        super.doPost(httpServletRequest, httpServletResponse);
+    }
+}
